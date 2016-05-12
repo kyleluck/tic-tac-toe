c661661576068fa971da084bb3d06dba17d8d7bf
Kyle Luck <luck.kyle@gmail.com>
2016-05-12 09:49:43 -0400

    adding 'thinking...' message

--- main.js
+++ main.js
@@ -4,9 +4,18 @@ $('.btn').click(function() {
   if (currentValue === '' && turn) {
     $(this).text('O');
     turn = false;
+    displayThinking();
+    setTimeout(hideThinking, 5000);
   }
   else if (currentValue === '' && turn === false) {
     $(this).text('X');
     turn = true;
   }
 });
+
+function displayThinking () {
+  $('.alert').show();
+}
+function hideThinking () {
+  $('.alert').hide();
+}
