0ce6f53f14bffa328b82880cbb68413fd53472d1
Kyle Luck <luck.kyle@gmail.com>
2016-05-12 09:34:38 -0400

    updating logic in main.js

--- main.js
+++ main.js
@@ -1,27 +1,12 @@
-var counterObj = {
-  "button1": 1,
-  "button2": 1,
-  "button3": 1,
-  "button4": 1,
-  "button5": 1,
-  "button6": 1,
-  "button7": 1,
-  "button8": 1,
-  "button9": 1
-};
+var turn = true;
 $('.btn').click(function() {
-  var name = $(this).attr("name");
   var currentValue = $(this).text();
-  if (currentValue === '') {
-    if (counterObj[name] % 3 === 0) {
-      $(this).text('');
-    }
-    else if (counterObj[name] % 2 === 1) {
-      $(this).text('O');
-    }
-    else if (counterObj[name] % 2 === 0){
-      $(this).text('X');
-    }
-    counterObj[name]++;
+  if (currentValue === '' && turn) {
+    $(this).text('O');
+    turn = false;
+  }
+  else if (currentValue === '' && turn === false) {
+    $(this).text('X');
+    turn = true;
   }
 });
