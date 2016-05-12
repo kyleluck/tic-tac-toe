38513c3d19f185c4094b3d6b6361c255ebf7392c
Kyle Luck <luck.kyle@gmail.com>
2016-05-12 09:25:47 -0400

    logic to place X and/or O

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-var counter = 1;
+var counterObj = {
+  "button1": 1,
+  "button2": 1,
+  "button3": 1,
+  "button4": 1,
+  "button5": 1,
+  "button6": 1,
+  "button7": 1,
+  "button8": 1,
+  "button9": 1
+};
 $('.btn').click(function() {
-  if (counter % 2 == 1) {
-    $(this).text('X');
+  var name = $(this).attr("name");
+  var currentValue = $(this).text();
+  if (currentValue === '') {
+    if (counterObj[name] % 3 === 0) {
+      $(this).text('');
+    }
+    else if (counterObj[name] % 2 === 1) {
+      $(this).text('O');
+    }
+    else if (counterObj[name] % 2 === 0){
+      $(this).text('X');
+    }
+    counterObj[name]++;
   }
-  else {
-    $(this).text('O');
-  }
-  counter++;
 });
